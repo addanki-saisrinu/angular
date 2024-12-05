0d8b3bd71fca154e937420e8342c6553792a409d
saiSrinu <saisrinuaddanki8@gmail.com>
2024-12-05 17:10:26 +0530

    passengers names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @Component({
+
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
+  passengers: any;
+  ngOnInit(): void {
+    this.passengers = [
+      {
+        name: 'John',
+        age: 25,
+        gender: 'Male'
+      },
+      {
+        name: 'Jane',
+        age: 30,
+        gender: 'Female'
+      },
+      {
+        name: 'Bob',
+        age: 35,
+        gender: 'Male'
+      },
+      {
+        name: 'Alice',
+        age: 20,
+        gender: 'Female'
+      }
+    ]
+  }
 }
